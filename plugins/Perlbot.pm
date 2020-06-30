a52105b2bea2f4070b4410b794de6bd14da2686b
Ryan Voots <simcop2387@simcop2387.info>
2020-06-29 20:24:13 -0400

    Tweak some seccomp rules, add newer perls to config, and improve behavior of evals.  This removes surprising behavior from use v5.XX not enabling strict properly when used in pastebins and evals due to the wrapper code that enables some other version specific features;

--- plugins/Perlbot.pm
+++ plugins/Perlbot.pm
@@ -26,14 +26,47 @@ package Acme::RandomOS;
   }
   sub FETCH {
     my ($p)=caller(1); 
-    if ($p eq 'File::Temp') {
+    if ($p eq 'File::Temp' || $p =~ /^Inline/) {
       return $Acme::RandomOS::__original
     } else {
-      my @os=qw/aix bsdos dynixptx darwin freebsd haiku linux hpux irix next openbsd dec_osf svr4 sco_sv unicos unicosmk solaris sunos MSWin32 dos os2 cygwin VMS vos os390 os400 posix-bc riscos amigaos macos 3b1 atari MSWinCE/; 
+#      my @os=qw/aix bsdos dynixptx darwin freebsd haiku linux hpux irix next openbsd dec_osf svr4 sco_sv unicos unicosmk solaris sunos MSWin32 dos os2 cygwin VMS vos os390 os400 posix-bc riscos amigaos macos 3b1 atari MSWinCE xenix MSWin16 MSWin63 WSL minix vxworks ecos cpm pdp-11/; 
+      my @os = qw/VMS/;
       $os[rand()*@os];
     } 
   };
   {my$hidden;tie$hidden,'Acme::RandomOS';$^O=$hidden};
+EOF
+
+my $ZathrasCode = <<'EOF';
+# This started out as a bad babylon 5 joke, now it's a weird 
+# meta class that captures all method calls and arguments
+# Slightly used to 
+do {
+    package
+    Zathras;
+    our $AUTOLOAD;
+    use overload '""' => sub { ## no critic
+        my $data = @{$_[0]{args}}? qq{$_[0]{data}(}.join(', ', map {"".$_} @{$_[0]{args}}).qq{)} : qq{$_[0]{data}};
+        my $old = $_[0]{old};
+
+        my ($pack, undef, undef, $meth) = caller(1);
+
+        if ($pack eq 'Zathras' && $meth ne 'Zahtras::dd_freeze') {
+            if (ref($old) ne 'Zathras') {
+                return "Zathras->$data";
+            } else {
+                return "${old}->$data";
+            }
+        } else {
+           $old = "" if (!ref($old));
+           return "$old->$data"
+        }
+      };
+    sub AUTOLOAD {$AUTOLOAD=~s/.*:://; bless {data=>$AUTOLOAD, args => \@_, old => shift}}
+    sub DESTROY {}; # keep it from recursing on destruction
+    sub dd_freeze {$_[0]=\($_[0]."")}
+    sub can {my ($self, $meth) = @_; return sub{$self->$meth(@_)}}
+    };
 EOF
 
 sub deparse_perl_code {
@@ -176,10 +209,30 @@ sub get_stdin {
 }
 
 sub perl_wrap_file {
-    my ($class, $lang, $code) = @_;
+    my ($class, $lang, $incode) = @_;
 
+    my $code = "";
     my $codefile = Path::Tiny->tempfile(TEMPLATE => "perl_code_XXXXXXXXXX", DIR => '/eval/', UNLINK => 0);
-    $codefile->spew($code);
+
+    my ($version) = ($lang =~ /^perl5\.([.0-9]*)t?$/);
+
+    if ($lang eq 'perl' || $lang eq 'perlt') {
+        # TODO this should be more dynamic
+        $code = "use v5.31; no strict; use feature qw/isa postderef refaliasing lexical_subs postderef_qq signatures/; use experimental 'declared_refs';\nuse experimental 'isa';\nuse experimental 'signatures';\n#line 1 \"(IRC)\"\n$incode";
+    } elsif ($version >= 8) {
+      $code = qq{use v5.$version;\nno strict;no warnings;\n};
+      if ($version >= 22) {
+        $code .= q{BEGIN{require strict; $^H &= ~strict->all_explicit_bits;};};
+      } elsif ($version > 16) {
+        $code .= q(BEGIN {$^H &= ~0xe0;};);
+      }
+      $code .= qq{\n#line 1 "(IRC)"\n$incode};
+
+    } else {
+      $code = $incode;
+    }
+
+    $codefile->spew_utf8($code);
 
     my $randfile = get_stdin();
 
@@ -190,15 +243,32 @@ sub perl_wrap_file {
     my $testfh = select;
     my $outfh = *STDOUT;
     my $errfh = *STDERR;
+    {local $@;
+    eval {
+    binmode($outfh, ":encoding(UTF-8)");
+    binmode($errfh, ":encoding(UTF-8)");
+    };
+    };
     my $randfile = '].$randfile.q[';
+
+    local $/="\n";
+    local $\;
+    local $,;
 
     my $AcmeCode= <<'EOF';
 ].$AcmeCode.q[
 EOF
 
+my $ZathrasCode= <<'EOF';
+].$ZathrasCode.q[
+EOF
+
     
   { local $@;
-    eval $AcmeCode;
+#    eval $AcmeCode;
+  }
+  { local $@;
+    eval $ZathrasCode;
   }
 
     eval {
@@ -210,12 +280,12 @@ EOF
     use Fcntl qw/SEEK_CUR/;
     sub systell { sysseek($_[0], 0, SEEK_CUR) }
 
-    my @tells = map {$_->flush(); (tell($_), systell($_))} $testfh, $outfh, $errfh;
+    my @tells = map {$_->flush(); (tell($_), systell($_))} $testfh, $outfh;
 
     my $value = do ']. $codefile .q[';
     my $err=$@;
 
-    my @nextells = map {$_->flush(); (tell($_), systell($_))} $testfh, $outfh, $errfh;
+    my @nextells = map {$_->flush(); (tell($_), systell($_))} $testfh, $outfh;
     ].($lang =~ /5\.6/? 'print $value' : q[
     if ($err) {
       print "ERROR: $err";
@@ -228,6 +298,7 @@ EOF
         local $Data::Dumper::Quotekeys = 0;
         local $Data::Dumper::Indent = 0;
         local $Data::Dumper::Useqq = 1;
+        local $Data::Dumper::Freezer = "dd_freeze";
         $value = ref($value) ? Dumper ($value) : "".$value;
         print $value;
       } 
